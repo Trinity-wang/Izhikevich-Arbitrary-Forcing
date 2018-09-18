ef1ac7a3cd8d3a9aec80a39a5b6c5b87eed13abc
Andrew Hansen <33072200+agh-nmi@users.noreply.github.com>
2018-09-18 16:01:28 -0700

    Update izhikevich_arbitrary_forcing.m

--- izhikevich_arbitrary_forcing.m
+++ izhikevich_arbitrary_forcing.m
@@ -88,7 +88,6 @@ stim = [zeros(1, stim_bnd(1)/sim_res), stim, zeros(1, (sim_bnd(2) - stim_bnd(2))
 
 %  A. EXECUTION
 for t = 1 : length(time) - 1
-    
     V = V + 0.25*(0.04*V^2 + 5*V + 140 - U + stim(t));
     U = U + 0.25*p(n, 1)*(p(n, 2)*V - U);
     
